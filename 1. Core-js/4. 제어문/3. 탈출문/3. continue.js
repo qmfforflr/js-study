52e1b13fa405cda9c9b63e2a5fb2f573038cfb4e
qmfforflr <qmfforflr@naver.com>
2021-02-22 21:35:50 +0900

    [21.02.21] #quiz 풀어보기

--- 1. Core-js/4. 제어문/3. 탈출문/3. continue.js
+++ 1. Core-js/4. 제어문/3. 탈출문/3. continue.js
@@ -8,8 +8,11 @@ for (var i = 1; i < 11; i++) {
 }
 
 while(true) {
-    var n = +prompt("정수를 입력하세요.")
-    if (n === 0) break;
+    var ran1 = Math.floor(Math.random() * 101);
+    var n = +prompt(`${ran1}정수를 입력하세요.`)
+    if (n === 0) {
+        break;
+    }
     else if (n === 1) continue;
 
     alert('메롱메롱~')
--- 1. Core-js/4. 제어문/3. 탈출문/test.js
+++ 1. Core-js/4. 제어문/3. 탈출문/test.js
@@ -1,15 +1,31 @@
-/*
-- Quiz. 
-아래 요구사항에 맞는 코드를 작성하고 
-브라우저에서 실행하여 테스트하세요.
-- 요구사항
-1. 프로그램 실행 시 0~100사이의 무작위 두 수의 합을 
-   물어보는 문제가 지속적으로 출제되게 하세요.
-2. 올바른 답을 입력할 시 “정답입니다”를 
-   틀린 답을 입력할 시 “오답입니다”를 출력하세요.
-3. 사용자가 0을 입력하면 문제 출제를 중단하고 
-   누적된 정답 횟수와 오답횟수를 출력하세요.
-*/
+// var correct = 0;
+// var wrong = 0;
+
+// while (true) {
+//    var ran1 = Math.floor(Math.random() * 101);
+//    var ran2 = Math.floor(Math.random() * 101);
+//    var answer = ran1 + ran2;
+
+//    var reply = +prompt(`${ran1} + ${ran2} = ??`);
+//    if (reply === 0) {
+//       alert(`프로그램이 종료되었습니다. 
+
+//          \n#정답 횟수: ${correct}회, 오답 횟수: ${wrong}회`);
+//       break;
+//    } else if (reply === answer) {
+//       alert("정답입니다!")
+//       correct++;
+//       continue;
+//    } else if (reply != answer) {
+//       alert("오답입니다!")
+//       wrong++;
+//       continue;
+//    }
+// }
+
+
+
+
 
 var correct = 0;
 var wrong = 0;
