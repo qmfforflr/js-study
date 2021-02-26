f8db0cdfb5ef062df0994cb2834627b471f23095
qmfforflr <qmfforflr@naver.com>
2021-02-26 14:09:44 +0900

    [21.02.26] #Core js

---
+++ 1. Core-js/10. let, const/3. const.js
@@ -0,0 +1,22 @@
+const x = 'hello';
+console.log(x);
+
+// for(i = 0; i < 10; i++) {
+//     let total += i;
+// }
+
+//상수 이름은 관례적으로 대문자로만 지정, 스네이크 케이스로 지정
+const EARTH_RADIUS = 4404040404044;
+
+//const와 객체(객체, 배열, 함수)
+const person = {
+    name: 'park',
+    age: 20
+};
+
+person.name = 'kim';
+person.quiz = '?'
+
+console.log(person);
+//객체 내부의 프로퍼티는 수정 가능
+
